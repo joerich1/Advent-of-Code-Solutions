3301422e9a577ae0623e705b4787328f1a7f22b7
Joseph Richardson <56328156+joerich1@users.noreply.github.com>
2021-12-01 09:40:18 -0500

    Update gozer.py

--- 2021 - Py/gozer.py
+++ 2021 - Py/gozer.py
@@ -7,10 +7,16 @@ def readlines(data, to_int=False, to_np=False, np_map=None):
     """
     if to_int:
         lines = [int(line) for line in data.split('\n')]
+
     else:
         lines = [line for line in data.split('\n')]
+
     if to_np:
         arr = np.empty(shape=(len(lines), len(lines[0])))
-        #for row in lines:
+        for row in range(len(lines)):
+            for char in range(len(row)):
+                arr[row, char] = np_map[lines[row, char]]
+
+        return arr
 
     return lines
